88b1c0eaef4ee36846d2b6e7d08b54a8e48ad16f
akhand <akhand3108patel@gmail.com>
2023-07-01 22:49:36 +0530

    Send Hardcoded Resposne

--- app/server.go
+++ app/server.go
@@ -9,14 +9,24 @@ import (
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	response := "+PONG\r\n"
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Error writing response: ", err.Error())
+	}
+
+	conn.Close()
+
 }
